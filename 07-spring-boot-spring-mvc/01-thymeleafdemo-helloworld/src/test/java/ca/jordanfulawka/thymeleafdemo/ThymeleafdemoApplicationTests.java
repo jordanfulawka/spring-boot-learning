4351870c4b4d798c10f5a4880fd1edb898d5c105
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-07-28 22:21:05 -0400

    start spring MVC, experimenting with thymeleaf and simple HTML forms

---
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/test/java/ca/jordanfulawka/thymeleafdemo/ThymeleafdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jordanfulawka.thymeleafdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
