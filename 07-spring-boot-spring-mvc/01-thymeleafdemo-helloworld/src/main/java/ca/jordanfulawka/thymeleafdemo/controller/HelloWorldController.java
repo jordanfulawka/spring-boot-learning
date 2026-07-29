b9120d22dd231eb76b439f64dfed8cb8c194f1b8
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-07-28 23:58:40 -0400

    working with form data binding

--- 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/ca/jordanfulawka/thymeleafdemo/controller/HelloWorldController.java
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/ca/jordanfulawka/thymeleafdemo/controller/HelloWorldController.java
@@ -1,13 +1,18 @@
 package ca.jordanfulawka.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
     // need a controller method to show initial HTML form
-    @RequestMapping("/showForm")
+    @GetMapping("/showForm")
     public String showForm() {
         return "helloworld-form";
     }
@@ -15,6 +20,32 @@ public class HelloWorldController {
     // need a controller method to process the HTML form
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // need a controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+        // read request parameter from HTML form
+        String name = request.getParameter("studentName");
+        // convert data to all caps
+        name = name.toUpperCase();
+        // add message
+        String result = "Yo " + name + "!";
+        // add message to the model
+        model.addAttribute("message", result);
+        return "helloworld";
+    }
+
+    @PostMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String name, Model model) {
+        // convert data to all caps
+        name = name.toUpperCase();
+        // add message
+        String result = "Hey my friend from v3! " + name + "!";
+        // add message to the model
+        model.addAttribute("message", result);
         return "helloworld";
     }
 
---
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/ca/jordanfulawka/thymeleafdemo/model/Student.java
@@ -0,0 +1,4 @@
+package ca.jordanfulawka.thymeleafdemo.model;
+
+public class Student {
+}
