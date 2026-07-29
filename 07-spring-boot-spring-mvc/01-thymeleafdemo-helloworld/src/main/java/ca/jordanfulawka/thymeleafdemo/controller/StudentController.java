a3c0c9205f29e4a40b84824880a0c310e4dc6e81
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-07-29 15:13:33 -0400

    data binding on text fields and drop-downs

---
+++ 07-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/ca/jordanfulawka/thymeleafdemo/controller/StudentController.java
@@ -0,0 +1,41 @@
+package ca.jordanfulawka.thymeleafdemo.controller;
+
+import ca.jordanfulawka.thymeleafdemo.model.Student;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @GetMapping("/showStudentForm")
+    public String showForm(Model model) {
+
+        // create new Student object
+        Student student = new Student();
+
+        // add student object to the model
+        model.addAttribute("student", student);
+        model.addAttribute("countries", countries);
+
+        return "student-form";
+    }
+
+    @PostMapping("/processStudentForm")
+    public String processForm(@ModelAttribute("student") Student student) {
+
+        // log the input data
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+
+        return "student-confirmation";
+    }
+}
