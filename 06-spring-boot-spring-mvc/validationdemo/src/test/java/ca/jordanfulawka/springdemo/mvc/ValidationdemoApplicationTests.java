cfd106202bf81e268082ba7eca81ebe84753bc9b
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-07-30 16:38:54 -0400

    adding spring mvc validation to forms

---
+++ 06-spring-boot-spring-mvc/validationdemo/src/test/java/ca/jordanfulawka/springdemo/mvc/ValidationdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.jordanfulawka.springdemo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidationdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
