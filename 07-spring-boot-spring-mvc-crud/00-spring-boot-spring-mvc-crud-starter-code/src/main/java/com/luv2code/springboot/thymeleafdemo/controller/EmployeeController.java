05a90b6c6e8ecc6c990c58a6a39112b67695c941
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-07-31 18:25:42 -0400

    init spring mvc crud application, add get employees endpoint and view

---
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.luv2code.springboot.thymeleafdemo.controller;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        // get the employees from db
+        List<Employee> employees = employeeService.findAll();
+        // add to the spring model
+        model.addAttribute("employees", employees);
+        return "list-employees";
+    }
+}
