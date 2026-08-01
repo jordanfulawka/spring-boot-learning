f183e757b0388fd58f997d5b4b646cac6e03b72f
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-08-01 10:46:34 -0400

    update employee form by reusing employee-form and making use of prepopulation using thymeleaf automatic getters

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -45,5 +42,15 @@ public class EmployeeController {
         employeeService.save(employee);
         // use a redirect to prevent duplicate submissions
         return "redirect:/employees/list";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int id, Model model) {
+        // get employee from the service
+        Employee employee = employeeService.findById(id);
+        // set employee in the model to prepopulate the form
+        model.addAttribute("employee", employee);
+        // send over our form
+        return "employees/employee-form";
     }
 }
