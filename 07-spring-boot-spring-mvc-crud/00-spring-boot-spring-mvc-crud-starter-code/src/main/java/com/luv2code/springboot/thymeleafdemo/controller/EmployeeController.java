f665377f247fc05025b5d3e5bf37b2a3d1970ae0
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-08-01 10:34:24 -0400

    implement add employee form and controller methods, generate custom query using Spring Data JPA

--- 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ 07-spring-boot-spring-mvc-crud/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -27,6 +29,21 @@ public class EmployeeController {
         List<Employee> employees = employeeService.findAll();
         // add to the spring model
         model.addAttribute("employees", employees);
-        return "list-employees";
+        return "employees/list-employees";
+    }
+
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model model) {
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employees/employee-form";
+    }
+
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+        // save the employee
+        employeeService.save(employee);
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/employees/list";
     }
 }
