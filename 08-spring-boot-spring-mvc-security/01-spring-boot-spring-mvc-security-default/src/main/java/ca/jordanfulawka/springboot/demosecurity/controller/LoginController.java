83dbc63f725d681e09f6c2ce3c6ba962d1e274ca
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-08-01 15:45:41 -0400

    restrict URLs based on roles, implement custom access denied page

--- 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/ca/jordanfulawka/springboot/demosecurity/controller/LoginController.java
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/ca/jordanfulawka/springboot/demosecurity/controller/LoginController.java
@@ -5,8 +5,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
+
     @GetMapping("/showMyLoginPage")
     public String showMyLoginPage() {
         return "fancy-login";
+    }
+
+    @GetMapping("/access-denied")
+    public String showAccessDenied() {
+        return "access-denied";
     }
 }
